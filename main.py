17014d508f52ddac92b507c5b5075e2b13d64010
Tahmid-Hossain-287 <mstahmid0@gmail.com>
2021-09-02 14:37:20 +0600

    updated, under construction

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
-import os
+import os, time
 from selenium import webdriver
+from selenium.webdriver.common import action_chains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Edge(r'./web_driver/msedgedriver.exe')
 
 driver.get('https://www.pythonanywhere.com')
-print('done')
+actions = action_chains.ActionChains(driver)
+
+
+try:
+    login = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "login_link"))
+    )
+    print(f'The type of login is {type(login)}')
+    login.click()
+    time.sleep(5)
+    driver.save_screenshot('screenshot.png')
+    driver.quit()
+
+except Exception as e:
+    print(e)
+    driver.quit()
